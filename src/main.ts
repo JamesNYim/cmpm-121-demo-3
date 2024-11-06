ab0c3aa5536fdd91bde6a5616064feb97addb76b
JamesNYim <101849565+JamesNYim@users.noreply.github.com>
2024-11-06 13:55:45 -0800

    Finished D3.A: Created baseline cache map

--- src/main.ts
+++ src/main.ts
@@ -23,35 +23,11 @@ const map = leaflet.map(document.getElementById("map")!, {
 // Add base layer
 leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-  attribution: '&copy; OpenStreetMap contributors',
+  attribution: "&copy; OpenStreetMap contributors",
 }).addTo(map);
 
 // Player's position
 const playerMarker = leaflet.marker(START_LOCATION, { title: "Your position" }).addTo(map);
-=======
-=======
->>>>>>> fdeb033 (D3.a Complete)
-  attribution: "&copy; OpenStreetMap contributors",
-}).addTo(map);
-
-// Player's position
-const playerMarker = leaflet.marker(START_LOCATION, { title: "Your position" })
-  .addTo(map);
-<<<<<<< HEAD
->>>>>>> 9ea5e02 (D3.a Complete)
-=======
-  attribution: "&copy; OpenStreetMap contributors",
-}).addTo(map);
-
-// Player's position
-const playerMarker = leaflet.marker(START_LOCATION, { title: "Your position" })
-  .addTo(map);
->>>>>>> 6f4cbc1 (D3.a Complete)
-=======
->>>>>>> fdeb033 (D3.a Complete)
 
 // Track player's coins
 let playerCoins = 0;
@@ -71,19 +47,7 @@ function spawnCache(i: number, j: number) {
   // Rectangle to represent cache
   const cache = {
     rect: leaflet.rectangle(bounds).addTo(map),
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    coins: Math.floor(Math.random() * 20),  // Each cache has a random number of coins
-=======
     coins: Math.floor(Math.random() * 20),
->>>>>>> 9ea5e02 (D3.a Complete)
-=======
-    coins: Math.floor(Math.random() * 20),
->>>>>>> 6f4cbc1 (D3.a Complete)
-=======
-    coins: Math.floor(Math.random() * 20),
->>>>>>> fdeb033 (D3.a Complete)
   };
 
   // Popup interaction
@@ -93,19 +57,7 @@ function spawnCache(i: number, j: number) {
       <div>Cache location [${i}, ${j}]. Coins: <span id="value">${cache.coins}</span></div>
       <button id="collect">Collect</button>
       <button id="deposit">Deposit</button>`;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> 9ea5e02 (D3.a Complete)
-=======
-
->>>>>>> 6f4cbc1 (D3.a Complete)
-=======
-
->>>>>>> fdeb033 (D3.a Complete)
     // Button interaction for collecting and depositing:
     popupDiv.querySelector("#collect")!.addEventListener("click", () => {
       if (cache.coins > 0) {
